32dbb146b96b4387e26e09b0213e0a74002ce473
Retina Kundu <retinakundu94@gmail.com>
2024-02-26 18:01:17 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+     "Otis",
+    "Garfield"
+]
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+// destructivelyRemoveLastCat();
+// console.log(cats);
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    const copyOfcats = [...cats]
+    copyOfcats.push(name)
+    return copyOfcats
+}
+function prependCat(name) {
+    const copyOfcats = [...cats]
+    copyOfcats.unshift(name)
+    return copyOfcats
+}
+function removeLastCat() {
+    const copyOfcats = [...cats]
+    copyOfcats.pop()
+    return copyOfcats
+}
+function removeFirstCat() {
+    const copyOfcats = [...cats]
+    copyOfcats.shift()
+    return copyOfcats
+}
